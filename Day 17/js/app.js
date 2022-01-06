e2bc6b09118d3d61b03c20248278b3d2ea2be3d3
irabinbhat <bhatravi984@gmail.com>
2022-01-06 12:39:10 +0545

    Day 17 Completed

---
+++ Day 17/js/app.js
@@ -0,0 +1,78 @@
+// Script Goes Here...
+const API_URL =
+  "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=3fd2be6f0c70a2a598f084ddfb75487c";
+const IMG_PATH = "https://image.tmdb.org/t/p/w1280";
+const SEARCH_API =
+  'https://api.themoviedb.org/3/search/movie?api_key=3fd2be6f0c70a2a598f084ddfb75487c&query="';
+
+const form = document.getElementById("form");
+const search = document.getElementById("search");
+const main = document.querySelector("main");
+const nextBtn = document.querySelector('.next');
+
+getMovies(API_URL);
+
+async function getMovies(url) {
+  const res = await fetch(url);
+  const data = await res.json();
+
+  showMovies(data.results);
+}
+
+function showMovies(movies) {
+
+  main.innerHTML = "";
+  movies.forEach((movie) => {
+    const { title, poster_path, vote_average, overview } = movie;
+
+    const movieEl = document.createElement("div");
+    movieEl.classList.add("movie");
+
+    movieEl.innerHTML = `<img src="${IMG_PATH + poster_path}">
+
+        <div class="movie-info">
+            <h3>${title}</h3>
+            <span class="rating ${ratingColor(
+              vote_average
+            )}">${vote_average}</span>
+        </div>
+
+        <div class="overview">
+            <h3>Overview</h3>
+            <p>${overview}</p>
+        </div>`;
+
+    main.appendChild(movieEl);
+  });
+}
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const searchText = search.value;
+
+  if (searchText && searchText !== "") {
+    getMovies(SEARCH_API + searchText);
+    search.value = "";
+  } else {
+    window.location.reload();
+  }
+});
+
+function ratingColor(rating) {
+  if (rating >= 7) {
+    return "green";
+  } else if (rating >= 5) {
+    return "orange";
+  } else {
+    return "red";
+  }
+}
+
+var page = 1
+
+nextBtn.addEventListener('click', ()=>{
+
+    page++;
+    getMovies(API_URL + `&page=${page}`)
+})
